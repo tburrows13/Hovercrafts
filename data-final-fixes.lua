36a7ec6b408421d466723cd7b69d56ac9af3390c
Tom Burrows <tomburrows13@icloud.com>
2024-11-05 01:09:43 +0000

    Remove no longer needed collision-mask-util workaround

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,22 +1,6 @@
 require("constants")
 local collision_mask_util = require("__core__.lualib.collision-mask-util")
 
-local default_masks = require("__core__/lualib/collision-mask-defaults")
-collision_mask_util.collect_prototypes_with_layer = function(layer)
-  local prototype_list = {}
-  for type, default_mask in pairs (default_masks) do
-    if data.raw[type] then
-      for name, entity in pairs (data.raw[type]) do
-        local entity_mask = entity.collision_mask or default_mask
-        if entity_mask.layers[layer] then
-          table.insert(prototype_list, entity)
-        end
-      end
-    end
-  end
-  return prototype_list
-end
-
 hcraft_entities = {
   ["hovercraft-collision"] = true,
   ["hovercraft"] = true,
